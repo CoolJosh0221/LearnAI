ac05176bb2e15729b16e0a3208939be58cfee90c
CoolJosh0221 <joshluffy99@gmail.com>
2022-12-01 22:12:17 +0800

    relu

---
+++ relu.py
@@ -0,0 +1,28 @@
+import math
+import matplotlib.pyplot as plt
+
+x = []
+dx = -20
+while dx <= 20:
+    x.append(dx)
+    dx += 0.1
+
+
+def ReLU(x):
+    if x < 0: return 0
+    else: return x
+
+
+px = [xv for xv in x]
+py = [ReLU(xv) for xv in x]
+
+
+plt.plot(px, py)
+ax = plt.gca()
+ax.spines['right'].set_color('none')
+ax.spines['top'].set_color('none')
+ax.xaxis.set_ticks_position('bottom')
+ax.spines['bottom'].set_position(('data',0))
+ax.yaxis.set_ticks_position('left')
+ax.spines['left'].set_position(('data',0))
+plt.show()
---
+++ test2.py
@@ -0,0 +1,11 @@
+import time
+import numpy as np
+
+x = np.random.random((20, 100))
+y = np.random.random((20, 100))
+
+t0 = time.time()
+for _ in range(1000):
+    z = x + y
+    z = np.maximum(z, 0.)
+print("Took: {0:.2f} seconds".format(time.time() - t0))
